4929f51d897cd2c944a8f8e8777a9a5a40bb9eed
denkach <denkach2211@gmail.com>
2022-10-15 22:09:33 +0000

    Deploying to gh-pages from @ denkach/goit-react-hw-08-phonebook@a02328342f3e9ae9b49b2a490234994c9e7fdaca 🚀

---
+++ static/js/206.66bc39fe.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[206],{1206:function(e,t,o){o.r(t);var r=o(1087),c=(o(2791),o(184));t.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsxs)("p",{className:"homeTitle",children:["To start working with the contact book, please"," ",(0,c.jsx)(r.OL,{to:"/register",style:{color:"white"},children:"REGISTER"})," ","or if you have an account -"," ",(0,c.jsx)(r.OL,{to:"/login",style:{color:"white"},children:"LOGIN"})]})})}}}]);
+//# sourceMappingURL=206.66bc39fe.chunk.js.map
